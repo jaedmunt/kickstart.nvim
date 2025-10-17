aacf9d400a266de61bda7fd7159727499c47805d
Jaedon Munton <jaedonmuntonwork@gmail.com>
2025-10-17 16:23:04 +0100

    molten, yazi, image

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,7 +5,12 @@
 --
 -- NOTE (JM): Define plugins directly in this file (not separate .lua files) to avoid loading errors
 
--- CSV View Plugin
+-- -- CSV View Plugin - for csv files. Open a csv and use CsvViewToggle to enable the view.
+
+-- Primary commands for the csvview.nvim plugin in Neovim are :CsvViewEnable, :CsvViewDisable, and :CsvViewToggle.
+--  These commands allow users to enable, disable, and toggle the CSV view mode, respectively.
+--  The :CsvViewToggle command can also accept options to customize the view, such as specifying a delimiter, display mode, or header line number.
+--  For example, :CsvViewToggle delimiter=, display_mode=border header_lnum=1 enables the CSV view with a comma delimiter, border display mode, and the first line as the header.
 local csvview_plugin = {
   'hat0uma/csvview.nvim',
   ft = { 'csv', 'tsv' },
@@ -17,7 +22,7 @@ local csvview_plugin = {
   end,
 }
 
--- Yazi Plugin
+-- Yazi Plugin - File explorer. Installed to use with CsvView
 local yazi_plugin = {
   'mikavilpas/yazi.nvim',
   dependencies = {
@@ -29,7 +34,45 @@ local yazi_plugin = {
   cmd = { 'Yazi' },
 }
 
+-- Image Plugin
+local image_plugin = {
+  '3rd/image.nvim',
+  dependencies = { 'vhyrro/luarocks.nvim' }, -- magick installed for rendering
+  build = false,
+  opts = {
+    processor = 'magick_cli', -- ImageMagick CLI backend
+  },
+  config = function()
+    require('image').setup {
+      backend = 'kitty', -- or 'ueberzug' / 'wezterm'
+      integrations = {
+        markdown = {
+          enabled = true,
+          only_render_image_at_cursor = false
+        }
+      }
+    }
+  end,
+}
+
+-- Jupyter Notebook plugin (Molten)
+local molten_plugin = {
+  'benlubas/molten-nvim',
+  build = ':UpdateRemotePlugins', -- Required for Jupyter integration
+  dependencies = {
+    '3rd/image.nvim', -- Enables inline image support if possible
+  },
+  config = function()
+    vim.g.molten_auto_open_output = true
+    vim.g.molten_image_provider = 'image' -- Use image.nvim for display of images in the notebook
+    vim.g.molten_wrap_output = true
+  end,
+  ft = { 'python', 'jupyter' },
+}
+
 return {
   csvview_plugin,
   yazi_plugin,
+  image_plugin,
+  molten_plugin,
 }
